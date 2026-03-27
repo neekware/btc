be677b7e8ba1d4749169cf259654748f3c541951
Val Neekman <val@neekware.com>
2026-03-26 20:54:07 -0400

    fix: pin next build root for vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-
 // Disable telemetry and analytics
 process.env.NEXT_TELEMETRY_DISABLED = "1";
 process.env.NEXT_PUBLIC_VERCEL_ANALYTICS_DISABLED = "1";
@@ -8,7 +6,9 @@ process.env.NEXT_PUBLIC_VERCEL_ANALYTICS_DISABLED = "1";
 const nextConfig = {
   reactStrictMode: true,
   // output: "export", // Commented out to allow next start
-  outputFileTracingRoot: path.join(__dirname, "../"),
+  turbopack: {
+    root: __dirname,
+  },
   images: {
     unoptimized: true,
   },
